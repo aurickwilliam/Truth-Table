18afda1737c3525e16e3ca537460924dc7428b0e
aurickwilliam <aurickwilliam21@gmail.com>
2024-02-16 15:51:31 +0800

    Format it like a Table

--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 no_variables = int(input('Enter number of variables: '))
 
 all_list = []
+header_list = ['p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+if no_variables > len(header_list):
+    print('Too much Variable')
+    quit()
+
 # Formula: 2^n / 2
 for col in range(no_variables):
     var = []
@@ -17,7 +23,13 @@ for col in range(no_variables):
             value = 0
     all_list.append(var)
 
-for lst in all_list:
-    for truth in lst:
-        print(truth)
+for header in range(no_variables):
+    print('  ' + header_list[header] + '\t', end='')
+print()
+index = 0
+for c in range(2 ** no_variables):
+    print('  ', end='')
+    for lst in all_list:
+        print(str(lst[index]) + "\t  ", end='')
     print()
+    index += 1
