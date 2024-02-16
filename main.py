5e3f766deb816f692d74e76edc9cd2270318f94b
aurickwilliam <aurickwilliam21@gmail.com>
2024-02-16 15:15:13 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import Column_Value as cv
+
+# Printing the truth table
+no_variables = int(input('Enter number of variables: '))
+
+all_list = []
+# Formula: 2^n / 2
+for col in range(no_variables):
+    var = []
+    value = 0
+    for row in range(2 ** (col + 1)):
+        no_val = (2 ** no_variables) // (2 ** (col + 1))
+        for x in range(no_val):
+            var.append(value)
+        if value == 0:
+            value = 1
+        elif value == 1:
+            value = 0
+    all_list.append(var)
+
+for lst in all_list:
+    for truth in lst:
+        print(truth)
+    print()
